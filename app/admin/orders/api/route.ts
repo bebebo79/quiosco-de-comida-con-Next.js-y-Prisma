ceb6e55b0a6a4c7a890af3ec630e261ea17e8542
bebebo79 <bebebo-carmen@hotmail.com>
2025-10-05 22:25:40 +0200

    fix

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -2,6 +2,8 @@
 
 import { prisma } from "@/src/lib/prisma"
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     const orders = await prisma.order.findMany({
           where : {
